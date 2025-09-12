59d5caedb76a3ff0a0c8ba0685255f3c513a49a2
Janus Developer <dev@retrievo.net>
2025-09-12 11:55:53 -0400

    leetcode 1768

---
+++ leetcode-1768.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class Solution(BaseModel):
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        output: str = ""
+
+        for i in range(max(len(word1), len(word2))):
+            if i < len(word1):
+                output += word1[i]
+            if i < len(word2):
+                output += word2[i]
+
+        return output
+
+
+if __name__ == "__main__":
+    print(Solution().mergeAlternately("abc", "pqr"))
