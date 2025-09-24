b4ba0f3af911879eae253e5071b6d341a3ed72f9
Janus Developer <dev@retrievo.net>
2025-09-24 13:33:54 -0400

    leetcode 334

--- src/leetcode_0334.py
+++ src/leetcode_0334.py
@@ -1,17 +1,15 @@
 class Solution:
     def increasing_triplet(self, nums: list[int]) -> bool:
         low = float("inf")
-        high = low
+        mid = float("inf")
 
         for i in range(len(nums)):
-            if nums[i] <= low:
-                low = nums[i]
-            elif nums[i] <= high:
-                high = nums[i]
-            else:
+            if nums[i] > mid:
                 return True
 
-        return False
+            if nums[i] <= low:
+                low = nums[i]
+            else:
+                mid = nums[i]
 
-    def function_under_test(self, nums: list[int]) -> bool:
-        return self.increasing_triplet(nums)
+        return False
